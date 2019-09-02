0f1565ef53a73cca94d5ae3909279b8e26d87c74
PaulloClara <paulloclara@gmail.com>
2019-09-02 10:42:58 -0300

    att q03

--- exercicio-01-vetores/q03.c
+++ exercicio-01-vetores/q03.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 
 int main () {
-  int vector[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 2, 4, 6, 8, 1, 3, 5, 7, 9};
   int len = 20;
+  int vector[len];
 
-  int s = 0;
+  int sum = 0;
 
-  for (int i = 0; i < len / 2; i++) {
-    s += (vector[i] - vector[len - i - 1]) * (vector[i] - vector[len - i - 1]);
+  for (int i = 0; i < len; i++) {
+    printf("Valor da posicao %d\n> ", i);
+    scanf("%d", &vector[i]);
   }
 
-  printf("\nS = %d\n\n", s);
+  for (int i = 0; i < len/2; i++) {
+    sum += (vector[i] - vector[len-i-1]) * (vector[i] - vector[len-i-1]);
+  }
 
-  // system ("pause");
+  printf("\nSoma = %d\n\n", sum);
+
+  // system("pause");
   return 0;
 }
