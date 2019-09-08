fbd6e5c703dbd397167cdfaa4c3b584666bc2161
PaulloClara <paulloclara@gmail.com>
2019-09-08 12:19:37 -0300

    add q11

---
+++ exercicio-03-strings/q11.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "libs/myStrings.h"
+
+#define MAXLEN 100
+
+int main() {
+  char str[MAXLEN];
+  size_t lenStr;
+
+  char substr[MAXLEN];
+  size_t lenSubstr;
+
+  char newSubstr[MAXLEN];
+  size_t lenNewSubstr;
+
+  char firstPart[MAXLEN/2];
+  char lastPart[MAXLEN/2];
+
+  char *strTemp;
+  size_t lenStrTemp;
+
+  fprintf(stdout, "\n\tDigite algo\n> ");
+  fgets(str, MAXLEN, stdin);
+  str[strlen(str)-1] = 0;
+  lenStr = strlen(str);
+
+  fprintf(stdout, "\n\tSubstring\n> ");
+  fgets(substr, MAXLEN, stdin);
+  substr[strlen(substr)-1] = 0;
+  lenSubstr = strlen(substr);
+
+  fprintf(stdout, "\n\tNova Substring\n> ");
+  fgets(newSubstr, MAXLEN, stdin);
+  newSubstr[strlen(newSubstr)-1] = 0;
+  lenNewSubstr = strlen(newSubstr);
+
+  strTemp = strstr(str, substr);
+  lenStrTemp = strlen(strTemp);
+
+  getSlice(str, firstPart, 0, lenStr-lenStrTemp);
+  getSlice(strTemp, lastPart, lenSubstr, lenStrTemp);
+
+  size_t lenNewStr = strlen(firstPart)+lenNewSubstr+strlen(lastPart);
+  char newStr[lenNewStr];
+
+  strcpy(newStr, firstPart);
+  strcat(newStr, newSubstr);
+  strcat(newStr, lastPart);
+
+  printf("\n\n\tString original => “%s“ [%d]\n", str, lenStr);
+  printf(
+    "\t“%s“ [%d] foi substituida por “%s“ [%d]\n",
+    substr, lenSubstr, newSubstr, lenNewSubstr
+  );
+  printf("\tA nova string é => “%s“ [%d]\n\n\n", newStr, lenNewStr);
+
+  return 0;
+}
